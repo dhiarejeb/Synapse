eba14e8e0614f98fae8ed441e16b1ba231a6ac2d
Dhia Rejeb <mohameddhia.rejeb@isimg.tn>
2026-02-05 22:57:30 +0100

    synapse fixing aws

--- synapse/src/main/java/com/synapse/synapse/config/S3Config.java
+++ synapse/src/main/java/com/synapse/synapse/config/S3Config.java
@@ -11,12 +11,21 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-    @Bean
+    /*@Bean
     public S3Client s3Client(
             @Value("${spring.aws.region}") String region
     ) {
         return S3Client.builder()
                 .region(Region.of(region))
                 .build(); // AWS SDK reads env vars automatically
+    }*/
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .credentialsProvider(
+                        software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider.create()
+                )
+                .region(Region.of(System.getenv("AWS_REGION")))
+                .build();
     }
 }
