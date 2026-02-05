518404e12f508215e26cd40f980a953de5e595e2
Dhia Rejeb <mohameddhia.rejeb@isimg.tn>
2026-02-05 23:51:06 +0100

    eeee prod

--- synapse-ui/src/environments/environment.prod.ts
+++ synapse-ui/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'http://synapse-api:8080'  // Docker internal backend
+  apiUrl: 'http://76.13.142.3:8080'  // VPS public IP, backend exposed on 8080
 };
+
+
