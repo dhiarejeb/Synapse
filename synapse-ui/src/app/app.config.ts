7047ad50eb183872dee52fd5ae1f62ae7459d380
Dhia Rejeb <mohameddhia.rejeb@isimg.tn>
2026-02-06 22:30:02 +0100

    Synapse V1.1

--- synapse-ui/src/app/app.config.ts
+++ synapse-ui/src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
 import {provideRouter, } from '@angular/router';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import {provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi} from '@angular/common/http';
+import {AuthBootstrapService} from './core/services/AuthBootstrapService';
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +15,12 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
 
-    provideHttpClient(withInterceptors([authInterceptor]),withFetch())
+    provideHttpClient(withInterceptors([authInterceptor]),withFetch()),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (auth: AuthBootstrapService) => () => auth.init(),
+      deps: [AuthBootstrapService],
+      multi: true
+    }
   ]
 };
